e226011319c754960815d91d56be103d2ab65903
JackLovel <gogkatsu@outlook.com>
2023-12-01 12:58:07 +0800

    add token some config

--- src/api/article.js
+++ src/api/article.js
@@ -1,6 +1,8 @@
-import request from '../utils/request.js'
+import request from '@utils/request.js'
+import { userTokenStore } from '../store/token'
 
 // 文章分类列表查询 
 export const articleCategoryListService = () => {
-    return request.get('/category')
+    const tokenStore = userTokenStore()
+    return request.get('/category', { headers: { 'Authorization': tokenStore.token } })
 }
--- src/utils/request.js
+++ src/utils/request.js
@@ -2,8 +2,9 @@ import axios from 'axios';
 const baseURL = '/api';
 const instance = axios.create({ baseURL })
 import { ElMessage } from 'element-plus';
+import { userTokenStore } from '../store/token';
 
-//添加请求拦截器
+//添加响应拦截器
 instance.interceptors.response.use(
     result => {
         if (result.data.code === 0) {
@@ -17,6 +18,21 @@ instance.interceptors.response.use(
     },
     err => {
         ElMessage.error('服务异常')
+        //请求错误的回调
+        return Promise.reject(err);
+    }
+)
+
+//添加请求拦截器
+instance.interceptors.request.use(
+    (config) => {
+        const tokenStore = userTokenStore();
+        if (tokenStore.token) {
+            config.headers.Authorization = tokenStore.token
+        }
+        return config
+    },
+    err => {
         //请求错误的回调
         return Promise.reject(err);
     }
